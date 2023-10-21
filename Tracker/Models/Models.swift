01b588b55c58b2e83f39b45ea7fa74401f9c08e0
lexx2406 <lexx2406@yandex.ru>
2023-10-21 14:58:47 +0400

    Исправления

--- Tracker/Models/Models.swift
+++ Tracker/Models/Models.swift
@@ -67,27 +67,6 @@ let emojiCollectionData = ["🙂", "😻", "🌺", "🐶", "❤️", "😱", "�
 
 
 let colorCollectionData = [
+    UIColor.Color_one, UIColor.Color_two, UIColor.Color_three, UIColor.Color_four, UIColor.Color_five, UIColor.Color_six, UIColor.Color_seven,UIColor.Color_eight, UIColor.Color_nine, UIColor.Color_ten, UIColor.Color_eleven,UIColor.Color_twelve, UIColor.Color_thirteen, UIColor.Сolor_fourteen, UIColor.Сolor_fiveteen,UIColor.Color_sixteen, UIColor.Color_seventeen, UIColor.Color_eighteen
 ]
 
-/*
-let colorCollectionData = [
-    UIColor(red: 0.992, green: 0.298, blue: 0.286, alpha: 1),
-    UIColor(red: 1, green: 0.533, blue: 0.118, alpha: 1),
-    UIColor(red: 0, green: 0.482, blue: 0.98, alpha: 1),
-    UIColor(red: 0.431, green: 0.267, blue: 0.996, alpha: 1),
-    UIColor(red: 0.2, green: 0.812, blue: 0.412, alpha: 1),
-    UIColor(red: 0.902, green: 0.427, blue: 0.831, alpha: 1),
-    UIColor(red: 0.976, green: 0.831, blue: 0.831, alpha: 1),
-    UIColor(red: 0.204, green: 0.655, blue: 0.996, alpha: 1),
-    UIColor(red: 0.275, green: 0.902, blue: 0.616, alpha: 1),
-    UIColor(red: 0.208, green: 0.204, blue: 0.486, alpha: 1),
-    UIColor(red: 1, green: 0.404, blue: 0.302, alpha: 1),
-    UIColor(red: 1, green: 0.6, blue: 0.8, alpha: 1),
-    UIColor(red: 0.965, green: 0.769, blue: 0.545, alpha: 1),
-    UIColor(red: 0.475, green: 0.58, blue: 0.961, alpha: 1),
-    UIColor(red: 0.514, green: 0.173, blue: 0.945, alpha: 1),
-    UIColor(red: 0.678, green: 0.337, blue: 0.855, alpha: 1),
-    UIColor(red: 0.553, green: 0.447, blue: 0.902, alpha: 1),
-    UIColor(red: 0.184, green: 0.816, blue: 0.345, alpha: 1)
-]
-*/
